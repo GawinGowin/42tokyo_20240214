249b4b82635b721aa30806141b7a6a9723a81879
kkaneta <knt.khe@proton.me>
2024-02-14 07:16:18 +0000

    Update product price font size and add image carousel

--- src/Products.tsx
+++ src/Products.tsx
@@ -1,21 +1,49 @@
+import React, { useState, useRef } from 'react';
 import products from '../products.json';
 import './Products.css';
+
+function ProductCard({ product }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const intervalIdRef = useRef(null); // useRefを使用してintervalIdを保存
+
+  const changeImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % product.image_urls.length);
+  };
+
+  return (
+    <div className="product-card">
+      <img
+        src={product.image_urls[currentIndex]}
+        alt={product.name}
+        className="product-image"
+        onMouseEnter={() => {
+          // マウスが画像の上にある時、setIntervalを使って画像を更新
+          if (!intervalIdRef.current) {
+            intervalIdRef.current = setInterval(changeImage, 500);
+          }
+        }}
+        onMouseLeave={() => {
+          // onMouseLeaveでタイマーをクリア
+          clearInterval(intervalIdRef.current);
+          intervalIdRef.current = null; // タイマーIDをリセット
+        }}
+      />
+      <div className="product-info">
+        <h2 className="product-name">{product.name}</h2>
+        <p className="product-price">{product.price}</p>
+        <p className="product-title">{product.title}</p>
+      </div>
+    </div>
+  );
+}
 
 function Products() {
   return (
     <>
+      <h1>商品一覧</h1>
       <div className="product-list">
         {products.map((product) => (
-          <div key={product.id} className="product-card">
-            <img src={product.image_urls[0]} alt={product.name} className="product-image" />
-			<img src={product.image_urls[1]} alt={product.name} className="product-image" />
-			<img src={product.image_urls[2]} alt={product.name} className="product-image" />
-            <div className="product-info">
-              <h2 className="product-name">{product.name}</h2>
-              <p className="product-price">{product.price}</p>
-              <p className="product-title">{product.title}</p>
-            </div>
-          </div>
+          <ProductCard key={product.id} product={product} />
         ))}
       </div>
     </>
@@ -23,3 +51,4 @@ function Products() {
 }
 
 export default Products;
+
